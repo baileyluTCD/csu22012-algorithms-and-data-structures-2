cb274103e04c057ea533e6f916966be15d70b972
Luke Bailey <baileylu@tcd.ie>
2025-02-20 17:27:00 +0000

    Wrote script for making submitty submissions

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,40 @@
 {pkgs, ...}: {
-  packages = with pkgs; [
-    jdt-language-server
-    nil
-    lemminx
-  ];
-
   languages.java = {
     enable = true;
     maven.enable = true;
   };
 
-  enterTest = ''
-    mvn test
-  '';
+  scripts.strip-submission = {
+    package = pkgs.nushell;
+    binary = "nu";
+    exec =
+      # bash
+      ''
+        # Produce a submitty prepared submission file
+        #
+        # Dumps java files with no package information into `submission/`
+        def main [location: path] {
+          let root = $env.DEVENV_ROOT
+          let submission = $root | path join submission
+
+          if not ( $location | path join pom.xml | path exists ) {
+              print "No pom.xml found at the specified location. Try switching to the root of a Maven project."
+              exit 1
+          }
+
+          rm $submission -rf
+          mkdir $submission
+
+          let java_files = glob ($location | path join src/main/java/**/*.java)
+          for file in $java_files {
+            let name = $file | path basename
+            let out = $submission | path join $name
+
+            open $file | str replace "package ie.tcd.baileylu;\n\n" "" | save $out
+          }
+
+          print "Successfully produced submission files"
+        }
+      '';
+  };
 }
